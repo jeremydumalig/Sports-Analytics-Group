d9d0e290258674d05d3226fb29ec7fe57fd98fd2
Jeremy Dumalig <jeremydumalig@gmail.com>
2023-01-30 18:01:28 -0600

    1/30

---
+++ Linear Regression/Tutorial3.R
@@ -0,0 +1,10 @@
+# Set working directory
+setwd("/Users/jeremydumalig/Documents/GitHub/Sports-Analytics-Group/Linear Regression")
+
+# Import tidyverse and ggplot2 packages
+library(tidyverse)
+library(ggplot2)
+
+# Import dataset
+nba <- read_csv("nba2022.csv")
+
