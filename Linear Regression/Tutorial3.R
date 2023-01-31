e798a2b9655689b647d59105d75bf3d80660c224
Jeremy Dumalig <jeremydumalig@gmail.com>
2023-01-30 18:49:21 -0600

    1/30 Update

--- Linear Regression/Tutorial3.R
+++ Linear Regression/Tutorial3.R
@@ -1,10 +1,44 @@
 # Set working directory
 setwd("/Users/jeremydumalig/Documents/GitHub/Sports-Analytics-Group/Linear Regression")
 
+# Install ggplot2 package
+# install.packages("modelr")
+
 # Import tidyverse and ggplot2 packages
 library(tidyverse)
 library(ggplot2)
+library(modelr)
 
-# Import dataset
-nba <- read_csv("nba2022.csv")
+# Import dataset, add points column
+nba <- 
+  read_csv("nba2022.csv") %>%
+  mutate(PTS = FTM + 3*`3PM` + 2*(FGM - `3PM`))
 
+# Visualize data
+ggplot(data=nba, aes(x=FTA, y=PTS)) +
+  geom_point()
+
+# Find correlation between two variables
+?cor
+cor(nba$FTA, nba$PTS)
+
+# Fit  data and find linear regression coefficients
+# Note that fit$coefficients and fit[["coefficients"]] return the same output
+?lm
+fit <- lm(data=nba, PTS ~ FTA)
+coef <- fit$coefficients
+coef <- fit[["coefficients"]] # returns the same output as above
+
+# Add predictions to nba dataframe
+# Note that add_predictions() effectively does the same as mutate()
+?add_predictions
+nba <- 
+  nba %>%
+  add_predictions(fit) %>%
+  mutate(Residual = PTS - pred)
+
+# Visualize data and regression fit
+ggplot(data=nba, aes(x=FTA, y=PTS)) +
+  geom_abline(intercept=coef['(Intercept)'], slope=coef['FTA'], color="blue") +
+  geom_segment(aes(x=FTA, xend=FTA, y=PTS, yend=pred), color = "light blue") +
+  geom_point()
